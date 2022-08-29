13d5b0a3bd50092b2764e76c047e8d42fe7a9e22
palch <palchuk.volodymyr@gmail.com>
2022-08-29 15:09:05 +0400

    fragments

---
+++ app/src/main/java/com/vladimir/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,105 @@
+package com.vladimir.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.vladimir.shoppinglist.data.ShopListRepositoryImpl
+import com.vladimir.shoppinglist.domain.AddShopItemUseCase
+import com.vladimir.shoppinglist.domain.EditShopItemUseCase
+import com.vladimir.shoppinglist.domain.GetShopItemUseCase
+import com.vladimir.shoppinglist.domain.ShopItem
+import java.lang.Exception
+
+class ShopItemViewModel : ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopItemUseCase = GetShopItemUseCase(repository)
+    private val addShopItemUseCase = AddShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+        get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+
+    private val _closeScreen = MutableLiveData<Unit>()
+    val closeScreen: LiveData<Unit>
+        get() = _closeScreen
+
+    fun getShopItem(shopItemId: Int) {
+        val item = getShopItemUseCase.getShopItem(shopItemId)
+        _shopItem.value = item
+    }
+
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = purseName(inputName)
+        val count = purseCount(inputCount)
+        val fieldsValidated = validateInput(name, count)
+        if (fieldsValidated) {
+            val shopItem = ShopItem(name, count, true)
+            addShopItemUseCase.addShopItem(shopItem)
+            finishWork()
+        }
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = purseName(inputName)
+        val count = purseCount(inputCount)
+        val fieldsValidated = validateInput(name, count)
+        if (fieldsValidated) {
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
+        }
+
+    }
+
+    private fun purseName(inputName: String?): String {
+        return inputName?.trim() ?: ""
+    }
+
+    private fun purseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+        if (name.isBlank()) {
+            result = false
+            _errorInputName.value = true
+        }
+        if (count <= 0) {
+            result = false
+            _errorInputCount.value = true
+        }
+        return result
+    }
+
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value = false
+    }
+
+    private fun finishWork() {
+        _closeScreen.value = Unit
+    }
+
+}
